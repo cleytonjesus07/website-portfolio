2f0ff0b58615ab7536c2c5b550e36153f322496e
Cleyton Jesus <cleytonjesus007@gmail.com>
2022-11-22 18:51:09 -0300

    update margin-top on tag nav

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="p-4 flex justify-between rounded-md">
+        <header className="p-4 flex justify-between rounded-md max-md:flex-col">
             <span className="font-black select-none">
                 Cleyton <span className="font-thin">Jesus</span>
             </span>
-            <nav>
-                <ul className="flex space-x-4">
+            <nav className="max-md:mt-5">
+                <ul className="block space-x-4 text-center">
                     <Link href={"#sobre"}>Sobre</Link>
                     <Link href={"#projetos"}>Projetos</Link>
                     <Link href={"#contato"}>Contato</Link>
