da7f0af70924a5e551f5bb06b2461f6405048ed3
Cleyton Jesus <cleytonjesus007@gmail.com>
2022-11-23 19:32:25 -0300

    update project

--- components/Banner/index.jsx
+++ components/Banner/index.jsx
@@ -22,22 +22,27 @@ export default function Banner() {
         return () => clearInterval(idInterval.current);
     }, [])
     return (
-        <div className="relative bg-slate-800 my-4 rounded-md p-4 max-md:flex-col flex justify-between  items-center h-[350px] ">
-            <div className="flex  items-center h-1/2">
+        <div className="relative bg-slate-800 my-4 rounded-md px-10 max-md:flex-col flex justify-between  items-center h-[350px] ">
+            <div className="flex items-center mt-5">
                 <h2 ref={h2Ref} className="text-5xl 
-                font-light ml-40 max-md:ml-0 select-none
+                font-light  
+                 select-none
                 text-center
+                ml-11
+                max-md:ml-0
                 ">
                 </h2>
             </div>
             {profilePic && (
-                <div style={{ background: `url(${profilePic})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', zIndex: 1 }} className=" absolute max-md:relative overflow-hidden rounded-full border-solid border-8 border-gray-800 -bottom-20 right-0
+                <div style={{ background: `url(${profilePic})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', zIndex: 1 }}
+                    className=" absolute overflow-hidden rounded-full border-solid border-8 border-gray-800 -bottom-20 
                 shadow-inner shadow-black
                 bg-gray-900
-                 w-[400px] h-[400px]
+                right-5
+                max-md:right-auto
+                 w-80 h-80
                  max-md:w-44 max-md:h-44
                 ">
-
                 </div>
             )}
         </div>
