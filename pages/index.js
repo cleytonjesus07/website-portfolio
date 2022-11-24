1d06971c19a3cd1569cd4a0527aff6470d2a512e
Cleyton Jesus <cleytonjesus007@gmail.com>
2022-11-24 13:21:29 -0300

    update meta tags

--- pages/index.js
+++ pages/index.js
@@ -175,7 +175,7 @@ function DrawsProjects({ images, isLoading, setIsLoading }) {
     <div className="flex  flex-wrap justify-center">
       {images.map(({ name, src, desc, link }, index) => {
         return (
-          <Link title="name" className="cover relative m-5 overflow-hidden flex w-80 h-40 justify-center items-center" key={index} href={link} target="_blank" referrerPolicy="no-referrer">
+          <Link title={name} className="cover relative m-5 overflow-hidden flex w-80 h-40 justify-center items-center" key={index} href={link} target="_blank" referrerPolicy="no-referrer">
             <span className="info absolute bg-black bg-opacity-90 h-full font-bold w-full flex flex-col items-center justify-center">{name[0].toUpperCase() + name.substr(1)}<p className="text-center mt-5 font-thin">{desc}</p></span>
 
             <span className={`absolute w-16 h-16 border-l-4 rounded-full animate-spin my-5 ${!isLoading ? 'hidden' : ''}`}></span>
