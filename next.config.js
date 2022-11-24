18951b107e7abe841a6f194a179c4935e69d5d8f
Cleyton Jesus <cleytonjesus007@gmail.com>
2022-11-23 22:33:08 -0300

    update meta tags

--- next.config.js
+++ next.config.js
@@ -1,12 +1,39 @@
 /** @type {import('next').NextConfig} */
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+`
+const securityHeaders =
+  [
+    {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff'
+    },
+    {
+      key: 'Content-Security-Policy',
+      value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+    }
+  ]
 const nextConfig = {
   reactStrictMode: true,
-  i18n:{
-    locales:['pt-BR'],
-    defaultLocale:'pt-BR'
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR'
   },
-  images:{
-    domains:["img.freepik.com"]
+  images: {
+    domains: ["img.freepik.com"]
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   }
 }
 
