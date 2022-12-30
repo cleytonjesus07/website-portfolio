e8c2ba1fd099609233e89bfd1360963b3dde7d8b
Cleyton Jesus <cleytonjesus007@gmail.com>
2022-12-29 23:09:08 -0300

    remove Image element

--- components/Banner/index.jsx
+++ components/Banner/index.jsx
@@ -2,25 +2,29 @@ import Image from "next/image";
 import { useEffect, useRef } from "react"
 
 export default function Banner({ dataUser }) {
-
     const profilePic = dataUser.avatar_url
     const h2Ref = useRef(), idInterval = useRef();
     useEffect(() => {
+        typewriter();
+        return () => clearInterval(idInterval.current);
+    }, [])
+
+    function typewriter() {
         const word = "Bem-vindo ao meu portfólio.😃";
         let index = 0;
+        const timeout = 1000;
         idInterval.current = setInterval(() => {
             if (index >= word.length) {
                 return clearInterval(idInterval.current);
             }
             h2Ref.current.innerHTML += word[index++]
-        }, 500);
+        }, timeout);
+    }
 
-        return () => clearInterval(idInterval.current);
-    }, [])
     return (
         <div className="relative bg-slate-800 my-4 rounded-md px-10 max-md:flex-col flex justify-between  items-center h-[350px] ">
             <div className="flex items-center mt-5">
-                <h2 ref={h2Ref} className="text-5xl 
+                <h2 ref={h2Ref} className="text-4xl 
                 font-light  
                  select-none
                 text-center
@@ -30,14 +34,27 @@ export default function Banner({ dataUser }) {
                 </h2>
             </div>
             {profilePic && (
-                <Image alt="profile picture" priority={true} src={profilePic} width={400} height={400} className="profile absolute overflow-hidden rounded-full border-solid border-8 border-gray-800 -bottom-20 
+                <>
+                    <div
+                        className={`
+                    profile absolute overflow-hidden rounded-full border-solid border-8 border-gray-800 -bottom-20 
                     shadow-inner shadow-black
                     bg-gray-900
                     right-5
                     max-md:right-auto
                     w-80 h-80
-                    max-md:w-44 max-md:h-44
-                " />
+                    max-md:w-44 
+                    max-md:h-44
+                    bg-cover
+                    bg-no-repeat
+                    bg-center
+                    `
+                        }
+                        style={{ backgroundImage: `url(${profilePic})` }}
+                    >
+                    </div>
+
+                </>
             )}
         </div>
     )
